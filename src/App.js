8fb21a39a1a344c938088fc9fdd3549cccda3e99
NguyenLong775 <nguyenquoclong070103@gmail.com>
2024-11-07 03:04:13 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Checkout from './components/Checkout';
+import Store from './components/Store';
+import Blank from './components/Blank';
+import Product from './components/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/blank" element={<Blank />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
